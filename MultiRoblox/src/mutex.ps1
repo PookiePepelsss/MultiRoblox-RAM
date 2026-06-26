bc922001466b938e9b4a2ae1dcfaf9115b5555d3
Pookie <125786325+PookiePepelsss@users.noreply.github.com>
2026-06-26 12:15:01 +1000

    Delete MultiRoblox directory

--- MultiRoblox/src/mutex.ps1
+++
@@ -1,4 +0,0 @@
-$null = New-Object Threading.Mutex($true, 'ROBLOX_singletonMutex')
-$null = New-Object Threading.Mutex($true, 'ROBLOX_singletonEvent')
-Write-Host 'MUTEX_HELD'
-while ($true) { Start-Sleep 3600 }
